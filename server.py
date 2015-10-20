c78ce8477699d584e88ff107a25f55790da6e0ee
Milosz <miloszw@kth.se>
2015-10-20 02:21:38 -0500

    Use underscore instead of camelCase

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 8080               # Arbitrary non-privileged port
 
 CRLF = '\r\n'
-HTTPVer = 'HTTP/1.1'
+HTTP_ver = 'HTTP/1.1'
 
 
 def serve():
@@ -23,33 +23,33 @@ def serve():
         conn, addr = sock.accept()              # Accept incoming connection
         print('Connection from', addr)
         data = conn.recv(1024)                  # Receive request
-        handleRequest(conn, data)               # Handle request
+        handle_request(conn, data)               # Handle request
         conn.close()                            # Close connection
         print('Closed connection')
 
-def handleRequest(conn, data):
-    decodedData = data.decode()
-    print('Request:', decodedData)
-    dataArray = decodedData.split(CRLF)
-    requestLine = dataArray[0]
-    headerLines = dataArray[1:]
+def handle_request(conn, data):
+    decoded_data = data.decode()
+    print('Request:', decoded_data)
+    data_array = decoded_data.split(CRLF)
+    request_line = data_array[0]
+    header_lines = data_array[1:] #TODO
 
     # TODO: check for trailing CRLF
-    method, uri, version = re.match(r'^(\S+)\s(\S+)\s(\S+)', requestLine).groups()
+    method, uri, version = re.match(r'^(\S+)\s(\S+)\s(\S+)', request_line).groups()
 
     # We only support GET requests
     if method != 'GET':
-        sendResponse(conn, 501)                 # 501: not implemented
+        send_response(conn, 501)                 # 501: not implemented
         return
     # We only support HTTP version 1.1
     if version != 'HTTP/1.1':
-        sendResponse(conn, 505)                 # 501: not implemented
+        send_response(conn, 505)                 # 501: not implemented
         return
 
     try:
         with open(uri[1:], 'rb') as f:
             content = f.read()
-            contentType = {
+            content_type = {
                 'txt': 'text/plain',
                 'jpg': 'image/JPEG',
                 'jpeg': 'image/JPEG',
@@ -57,36 +57,36 @@ def handleRequest(conn, data):
                 'htm': 'text/html'
             }.get(f.name.split('.')[1], 'text/plain')
             headers = {
-                'Content-Type': contentType,
+                'Content-Type': content_type,
                 'Content-Length': len(content),
                 'Last-Modified': time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.gmtime(osp.getmtime(f.name)))
             }
-            sendResponse(conn, 200, content, headers)
+            send_response(conn, 200, content, headers)
     except IOError:
-        sendResponse(conn, 404)
+        send_response(conn, 404)
 
-def sendResponse(conn, statusCode, messageBody='', messageHeaders={}):
+def send_response(conn, status_code, message_body='', message_headers={}):
     # Prepare status line
-    statusMessages = {
+    status_messages = {
         200: 'OK',
         404: 'Not Found',
         501: 'Not Implemented',
         505: 'HTTP Version not supported'
     }
-    statusString = '{} {}'.format(statusCode, statusMessages.get(statusCode,''))
-    statusLine = '{} {}{}'.format(HTTPVer, statusString, CRLF)
+    status_string = '{} {}'.format(status_code, status_messages.get(status_code,''))
+    status_line = '{} {}{}'.format(HTTP_ver, status_string, CRLF)
 
     # Prepare response headers
-    messageHeaders.update({
+    message_headers.update({
         'Server': 'ServerMcAwesome/0.1',
         'Date': time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.gmtime()),
         'Connection': 'close'
     })
-    headers = ''.join(['{}: {}{}'.format(k,v,CRLF) for (k,v) in messageHeaders.items()])
+    headers = ''.join(['{}: {}{}'.format(k,v,CRLF) for (k,v) in message_headers.items()])
 
     # Prepare response
-    response = bytearray('{}{}{}'.format(statusLine, headers, CRLF), 'utf-8')
-    response.extend(messageBody)
+    response = bytearray('{}{}{}'.format(status_line, headers, CRLF), 'utf-8')
+    response.extend(message_body)
     conn.sendall(response)
 
 def shutdown():
