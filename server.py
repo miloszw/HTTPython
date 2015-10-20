d7fccb1937f62d2ccc6fc95d52624f32848ca804
Milosz <miloszw@kth.se>
2015-10-20 04:28:48 -0500

    Add comments

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ def serve():
     while True:
         conn, addr = sock.accept()              # Accept incoming connection
         print('Connection from', addr)
-        data = conn.recv(1024)                  # Receive request
+        data = conn.recv(1024)                  # Receive request as binary data
         try:
             handle_request(conn, data)          # Handle request
         finally:
@@ -33,47 +33,49 @@ def serve():
         print('Closed connection')
 
 def handle_request(conn, data):
-    decoded_data = data.decode()
+    decoded_data = data.decode()                # Decode request to string
     print('Request:', decoded_data)
-    data_array = decoded_data.split(CRLF)
+    data_array = decoded_data.split(CRLF)       # Parse request into array by each line/header
     request_line = data_array[0]
     header_lines = data_array[1:]
+
+    # Check for trailing CRLF and Host header
     header_keys = map(lambda s: s.split(':')[0], header_lines)
-    # check for CRLF and Host header
     if data_array[-2:] != ['', ''] or 'Host' not in header_keys:
         send_response(conn, 400)                 # 400: bad request
         return
 
+    # Extract request method, resource uri and HTTP version
     try:
         method, uri, version = re.match(r'^(\S+)\s(\S+)\s(\S+)', request_line).groups()
-    except AttributeError: # match failed
+    except AttributeError:                       # Failed to match
         send_response(conn, 400)                 # 400: bad request
         return
-
-    # We only support GET requests
-    if method != 'GET':
+    if method != 'GET':                          # We only support GET requests
         send_response(conn, 501)                 # 501: not implemented
         return
-    # We only support HTTP version 1.1
-    if version != 'HTTP/1.1':
+    if version != 'HTTP/1.1':                    # We only support HTTP version 1.1
         send_response(conn, 505)                 # 505: version not supported
         return
 
     try:
-        with open(uri[1:], 'rb') as f:
+        with open(uri[1:], 'rb') as f:          # Try to open requested resource
             content = f.read()
-            content_type = {
+            content_type = {                    # Guess type based on extension, default to 'text/plain'
                 'txt': 'text/plain',
                 'jpg': 'image/JPEG',
                 'jpeg': 'image/JPEG',
                 'html': 'text/html',
                 'htm': 'text/html'
             }.get(f.name.split('.')[1], 'text/plain')
-            last_modified = time.gmtime(osp.getmtime(f.name))
+            last_modified = time.gmtime(osp.getmtime(f.name))       # Get file's last modified date
+
+            # Check if client passed along the 'If-Modified-Since' header, in which case compare that date
+            # with the last modified date of the file on the server, and send the file only if it's newer.
             if 'If-Modified-Since' in header_keys:
                 since = [header for header in header_lines if header.startswith('If-Modified-Since')][0]
                 since = since.split(':',1)[1].lstrip()
-                date_formats = [
+                date_formats = [                                    # Try all valid HTTP date formats
                     '%a %b %d %H:%M:%S %Y',
                     '%a, %d %b %Y %H:%M:%S GMT',
                     '%A, %d-%b-%y %H:%M:%S GMT'
@@ -85,18 +87,18 @@ def handle_request(conn, data):
                         continue
                     else:
                         if since > last_modified:
-                            send_response(conn, 304)
+                            send_response(conn, 304)                # 304: not modified.
                             return
                         else:
                             break
-            headers = {
+            headers = {                                             # Otherwise send file + relevant headers
                 'Content-Type': content_type,
                 'Content-Length': len(content),
                 'Last-Modified': time.strftime('%a, %d %b %Y %H:%M:%S GMT', last_modified)
             }
-            send_response(conn, 200, content, headers)
+            send_response(conn, 200, content, headers)              # 200: ok
     except FileNotFoundError:
-        send_response(conn, 404)
+        send_response(conn, 404)                                    # 404: not found
 
 def send_response(conn, status_code, message_body='', message_headers={}):
     # Prepare status line
