c579d0e8195875fb96840579d4066133918c649f
Milosz <miloszw@kth.se>
2015-10-19 21:50:02 -0500

    Serve forever, handle sigint

--- server.py
+++ server.py
@@ -1,15 +1,34 @@
-# Echo server program
 import socket
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 8080               # Arbitrary non-privileged port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-conn, addr = s.accept()
-print('Connected by', addr)
-while True:
-    data = conn.recv(1024)
-    if not data: break
-    conn.sendall(data)
-conn.close()
+
+
+def serve():
+    # Create TCP socket, bind it to given port and start listening for connections
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(0)
+    print('Listening on port {}'.format(PORT))
+
+    # Keep serving
+    while True:
+        conn, addr = s.accept()                 # Accept incoming connection
+        print('Connection from', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            conn.sendall(data)
+            print('Sent data')
+        conn.close()                            # Close connection
+        print('Closed connection')
+
+def shutdown():
+    print('Exiting...')
+    return
+
+if __name__ == '__main__':
+    try:
+        serve()
+    except KeyboardInterrupt:
+        shutdown()
